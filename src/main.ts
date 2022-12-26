27dfb8e8b44a341fde604c4ea3695a587cc4e666
Nguyen Duy Long <68091468+llong2195@users.noreply.github.com>
2022-12-26 22:54:53 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({origin: "*"});
+  app.enableCors({credentials: true, origin: ["https://my-blog-fe.vercel.app/","https://*.vercel.app/*","*" ]});
 
   await app.listen(PORT, () => console.log(`Server running om port: ${PORT}`));
 
